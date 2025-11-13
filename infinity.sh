5fb7c3a52f8c00c61b00d3cfbc156e545267bee2
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-11-13 22:00:52 +0700

    Update infinity.sh
    Initial for infinity-x 3.x

--- infinity.sh
+++ infinity.sh
@@ -6,7 +6,7 @@
 rm -rf .repo/local_manifests
 
 # Initialize repo for Infinity-X
-repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 15 -g default,-mips,-darwin,-notdefault
+repo init --no-repo-verify --git-lfs -u https://github.com/ProjectInfinity-X/manifest -b 16 -g default,-mips,-darwin,-notdefault
 
 # Sync
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync
@@ -16,9 +16,10 @@ rm -rf hardware/google/camera
 
 # Clone device/vendor/kernel repositories
 git clone https://github.com/LineageOS/android_hardware_google_camera --depth=1 hardware/google/camera
-git clone https://github.com/Hans982/android_device_google_coral device/google/coral -b lineage-22.2
-git clone https://github.com/Hans982/android_device_google_gs-common device/google/gs-common -b lineage-22.2
-git clone https://github.com/Hans982/android_kernel_google_msm-4.14 kernel/google/msm-4.14 -b lineage-22.2
+git clone https://github.com/han-senpai/device_google_coral device/google/coral -b bka
+git clone https://github.com/Hans982/android_device_google_gs-common device/google/gs-common -b lineage-23.0
+git clone https://github.com/han-senpai/kernel_google_msm-4.14 kernel/google/msm-4.14 -b bka
+git clone https://github.com/han-senpai/vendor_google_flame vendor/google/flame -b bka
 
 # Build Environment
 export BUILD_USERNAME=Hans982; \
@@ -27,4 +28,4 @@ export TZ=Asia/Tokyo; \
 
 # Build
 source build/envsetup.sh 
-lunch infinity_flame-userdebug && mka bacon
+lunch infinity_flame-userdebug && m bacon
