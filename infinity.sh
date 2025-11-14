4c3daec78f4d71755c02bedc2a171be88a51ee65
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-11-14 20:39:11 +0700

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -33,6 +33,7 @@ git clone https://github.com/han-senpai/device_google_coral device/google/coral 
 git clone https://github.com/Hans982/android_device_google_gs-common device/google/gs-common -b lineage-23.0
 git clone https://github.com/han-senpai/kernel_google_msm-4.14 kernel/google/msm-4.14 -b bka
 git clone https://github.com/han-senpai/vendor_google_flame vendor/google/flame -b bka
+git clone https://github.com/LineageOS/android_packages_apps_ElmyraService packages/apps/ElmyraService -b lineage-23.0
 
 # Build Environment
 export BUILD_USERNAME=Hans982; \
