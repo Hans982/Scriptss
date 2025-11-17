6b2a036c6d20b53431c3d8018716b7e91b6f00f2
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-11-17 11:02:45 +0700

    Update infinity.sh

--- infinity.sh
+++ infinity.sh
@@ -28,7 +28,7 @@ rm -rf out/target/product/flame/product
 # Clone device/vendor/kernel repositories
 git clone https://github.com/han-senpai/device_google_coral device/google/coral -b bka
 git clone https://github.com/Hans982/android_device_google_gs-common device/google/gs-common -b lineage-23.0
-git clone --depth 1 https://github.com/han-senpai/kernel_google_msm-4.14 kernel/google/msm-4.14 -b lineage-23.0
+git clone --depth 1 https://github.com/han-senpai/android_kernel_google_msm-4.14 kernel/google/msm-4.14 -b lineage-23.0
 git clone https://github.com/han-senpai/vendor_google_flame vendor/google/flame -b bka
 git clone https://github.com/LineageOS/android_packages_apps_ElmyraService packages/apps/ElmyraService -b lineage-23.0
 
