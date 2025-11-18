cbc016a17d830d5e0b733b000370f78713958d36
Hans982 <142167463+Hans982@users.noreply.github.com>
2025-11-18 12:33:03 +0700

    Update infinity.sh
    Bringback aperture and drop GoogleCamera!

--- infinity.sh
+++ infinity.sh
@@ -20,6 +20,7 @@ rm -rf device/google/gs-common
 rm -rf device/google/coral
 rm -rf vendor/google/flame
 rm -rf kernel/google/msm-4.14
+rm -rf hardware/google/camera
 
 # Cleaning out dir
 rm -rf out/target/product/flame/system
@@ -28,9 +29,10 @@ rm -rf out/target/product/flame/product
 # Clone device/vendor/kernel repositories
 git clone https://github.com/han-senpai/device_google_coral device/google/coral -b bka
 git clone https://github.com/Hans982/android_device_google_gs-common device/google/gs-common -b lineage-23.0
-git clone --depth 1 https://github.com/han-senpai/android_kernel_google_msm-4.14 kernel/google/msm-4.14 -b lineage-23.0
+git clone https://github.com/han-senpai/android_kernel_google_msm-4.14 --depth=1 kernel/google/msm-4.14 -b lineage-23.0
 git clone https://github.com/han-senpai/vendor_google_flame vendor/google/flame -b bka
 git clone https://github.com/LineageOS/android_packages_apps_ElmyraService packages/apps/ElmyraService -b lineage-23.0
+git clone https://github.com/LineageOS/android_hardware_google_camera --depth=1 hardware/google/camera -b lineage-23.0
 
 # Build Environment
 export BUILD_USERNAME=Hans982; \
